3ff113715bddf6dc2fed41b6bce8f77ad218639b
monchoduoc <165222335+monchoduoc@users.noreply.github.com>
2024-10-25 19:53:49 -0300

    Add files via upload
    código de movimientos y cambios de temperatura del sensor de humedad de nuestro proyecto (húmedo o seco) con su relativas temperaturas.

---
+++ tempratura.py
@@ -0,0 +1,21 @@
+import adafruit_dht
+import board
+import time
+
+# Configura el sensor DHT
+sensor = adafruit_dht.DHT11(board.D27)  # Asegurate de que el pin sea el correcto
+
+while True:
+    try:
+        # Lee la humedad y la temperatura
+        humedad = sensor.humidity
+        temperatura = sensor.temperature
+        if humedad is not None and temperatura is not None:
+            print(f'Temperatura={temperatura:.2f}*C  Humedad={humedad:.2f}%')
+        else:
+            print('Fallo la lectura del sensor. Intentar de nuevo')
+    except RuntimeError as error:
+        # Muestra errores de lectura
+        print(f'Error al leer el sensor: {error}')
+    
+    time.sleep(5)
